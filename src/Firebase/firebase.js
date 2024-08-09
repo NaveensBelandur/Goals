ebce1835a6368cd3ebaa76d5e6ce10990d920e39
NaveensBelandur <65352859+NaveensBelandur@users.noreply.github.com>
2024-08-09 17:31:34 +0530

    Update firebase.js

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -5,12 +5,7 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVP6PemYd1bSJF2LK77eu3rKzb46gtnt4",
-  authDomain: "goalfinal-dd30a.firebaseapp.com",
-  projectId: "goalfinal-dd30a",
-  storageBucket: "goalfinal-dd30a.appspot.com",
-  messagingSenderId: "980769186566",
-  appId: "1:980769186566:web:ed3d23e27f2e713269f487"
+ // Add This Config 
 };
 
 
